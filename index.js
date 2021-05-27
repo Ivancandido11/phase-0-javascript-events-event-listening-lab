19e1e0868b470c4df9eced421f661a9375317847
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-27 15:11:16 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const divFive = document.querySelector("body > div:nth-child(5) > div > div > div > div")
+    divFive.id = "five"
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    five.addEventListener('click', clickAlert)
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    }); 
 }
+addingEventListener()
